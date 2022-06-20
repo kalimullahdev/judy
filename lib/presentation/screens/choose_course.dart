8e43f3c4593caa27eb86506c7f2a2ca1c9c821c0
Kalimullah <Kalimullahdev@gmail.com>
2022-06-20 22:53:21 +0500

    choose course screen

--- lib/presentation/screens/choose_course.dart
+++ lib/presentation/screens/choose_course.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:judy_flutter_ui/utill/dimensions.dart';
 
-class ChooseCourse extends StatelessWidget {
+class ChooseCourse extends StatefulWidget {
   const ChooseCourse({Key? key}) : super(key: key);
+
+  @override
+  State<ChooseCourse> createState() => _ChooseCourseState();
+}
+
+class _ChooseCourseState extends State<ChooseCourse> {
+  final List<String> courseList = <String>[
+    "Business",
+    "Computer Science",
+    "Math and Logic",
+    "Language Learning",
+  ];
+
+  String currentCourseValue = "Language Learning";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,121 +63,78 @@ class ChooseCourse extends StatelessWidget {
               const SizedBox(
                 height: Dimensions.PADDING_SIZE_DEFAULT,
               ),
-              ExpansionTile(
-                textColor: Colors.black,
-                children: [
-                  ListTile(
-                    leading: Radio(
-                      value: 1,
-                      groupValue: 22,
-                      onChanged: (value) {},
-                    ),
-                    title: const Text("Leadership and Management"),
-                  ),
-                  ListTile(
-                    leading: Radio(
-                      activeColor: Colors.green,
-                      value: 1,
-                      groupValue: 1,
-                      onChanged: (value) {},
-                    ),
-                    title: const Text("Finance"),
-                  ),
-                  ListTile(
-                    leading: Radio(
-                      value: 1,
-                      groupValue: 22,
-                      onChanged: (value) {},
-                    ),
-                    title: const Text("Marketing"),
-                  ),
-                  ListTile(
-                    leading: Radio(
-                      value: 1,
-                      groupValue: 22,
-                      onChanged: (value) {},
-                    ),
-                    title: const Text("Entrepreneurship"),
-                  ),
-                ],
-                trailing: const Icon(
-                  Icons.radio_button_on,
-                  color: Colors.green,
-                ),
-                title: ListTile(
-                  leading: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff8A8A8F),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
+              ListView.builder(
+                shrinkWrap: true,
+                itemCount: courseList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ExpansionTile(
+                    textColor: Colors.black,
+                    children: [
+                      ListTile(
+                        leading: Radio(
+                          value: 1,
+                          groupValue: 22,
+                          onChanged: (value) {},
+                        ),
+                        title: Text(courseList[index]),
+                      ),
+                      ListTile(
+                        leading: Radio(
+                          activeColor: Colors.green,
+                          value: 1,
+                          groupValue: 1,
+                          onChanged: (value) {},
+                        ),
+                        title: const Text("Finance"),
+                      ),
+                      ListTile(
+                        leading: Radio(
+                          value: 1,
+                          groupValue: 22,
+                          onChanged: (value) {},
+                        ),
+                        title: const Text("Marketing"),
+                      ),
+                      ListTile(
+                        leading: Radio(
+                          value: 1,
+                          groupValue: 22,
+                          onChanged: (value) {},
+                        ),
+                        title: const Text("Entrepreneurship"),
+                      ),
+                    ],
+                    trailing: SizedBox(
+                      width: 20,
+                      height: 20,
+                      child: Icon(
+                        currentCourseValue == courseList[index]
+                            ? Icons.radio_button_on
+                            : Icons.radio_button_unchecked,
+                        color: Colors.green,
                       ),
                     ),
-                  ),
-                  title: const Text("Business"),
-                  subtitle: const Text("538 courses"),
-                ),
-              ),
-              ExpansionTile(
-                trailing: const Icon(
-                  Icons.radio_button_off,
-                  color: Colors.green,
-                ),
-                title: ListTile(
-                  leading: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff8A8A8F),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
+                    title: ListTile(
+                      leading: Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          color: Color(0xff8A8A8F),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(16),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  title: const Text("Computer Science"),
-                  subtitle: const Text("273 courses"),
-                ),
-              ),
-              ExpansionTile(
-                trailing: const Icon(
-                  Icons.radio_button_off,
-                  color: Colors.green,
-                ),
-                title: ListTile(
-                  leading: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff8A8A8F),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
+                      title: GestureDetector(
+                        onTap: () => setState(() {
+                          currentCourseValue = courseList[index];
+                        }),
+                        child: Text(courseList[index]),
                       ),
+                      subtitle: const Text("538 courses"),
                     ),
-                  ),
-                  title: const Text("Math and Logic"),
-                  subtitle: const Text("842 courses"),
-                ),
-              ),
-              ExpansionTile(
-                trailing: const Icon(
-                  Icons.radio_button_off,
-                  color: Colors.green,
-                ),
-                title: ListTile(
-                  leading: Container(
-                    width: 50,
-                    height: 50,
-                    decoration: const BoxDecoration(
-                      color: Color(0xff8A8A8F),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(16),
-                      ),
-                    ),
-                  ),
-                  title: const Text("Language Learning"),
-                  subtitle: const Text("239 courses"),
-                ),
+                  );
+                },
               ),
             ],
           ),
